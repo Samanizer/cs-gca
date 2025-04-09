10710fd04343f57ead0296aab7a941bf3480796d
Sam Dhiman <samanizer@gmail.com>
2025-04-09 13:42:38 -0700

    2nd dev

--- cyclic-shift.py
+++ cyclic-shift.py
@@ -5,3 +5,13 @@ array sorted in non-decreasing order.
 If it’s not possible, return -1.
 '''
 
+def min_cyclic_shift(arr):
+    n = len(arr)
+    target = sorted(arr)
+    
+    for i in range(n):
+        rotated = arr[i:] + arr[:i]
+        if rotated == target:
+            return i
+    
+    return -1
